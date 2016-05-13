a19837b37f4d9a7b5108ae8f2e5e83d16ef3e6d6
fanzhongrui <fanzhongrui@ict.ac.cn>
2016-05-13 10:23:10 +0800

    Create validParentheses.java

---
+++ algorithm/stack/validParentheses.java
@@ -0,0 +1,41 @@
+package leet.stack;
+
+import java.util.Stack;
+/**
+ * Given a string containing just the characters '(',')','{','}','[',']',
+ * determine if the input string is valid.
+ * The brackets must close in the correct order."()"and "()[]"are all valid
+ * but "(]" and "([)]"are not.
+ */
+public class validParentheses {
+	private static boolean isValid(String s){
+		Stack<Character> stack = new Stack();
+		for(int i=0; i<s.length(); i++){
+			char c = s.charAt(i);
+			switch(c){
+			case '(': stack.push(c);continue;
+			case '{': stack.push(c);continue;
+			case '[': stack.push(c);continue;
+			case ')': 
+					if(stack.pop().equals('(')) continue ;
+					else return false;
+			case '}': 
+					if(stack.pop().equals('{')) continue ;
+					else return false;
+			case ']': 
+					if(stack.pop().equals('[')) continue ;
+					else return false;
+			default:return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args){
+		String s1 = "((({{{[]}}})))";
+		System.out.println(validParentheses.isValid(s1));
+		String s2 = "(((){}{}))";
+		System.out.println(validParentheses.isValid(s2));
+		String s3 = "((({{[]}))})";
+		System.out.println(validParentheses.isValid(s3));
+	}
+}
